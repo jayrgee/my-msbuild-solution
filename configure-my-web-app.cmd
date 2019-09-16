f95299fc21a96d81ff51759b0424f9fc44eb2ff6
jayrgee <jayrgee@fastmail.fm>
2019-09-16 18:02:48 +1000

    revert msbuildexe to 4.0.30319; specify tv in rsp

--- configure-my-web-app.cmd
+++ configure-my-web-app.cmd
@@ -1,4 +1,4 @@
 @echo off
 ::set msbuildexe="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set msbuildexe="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
-%msbuildexe% configure-my-web-app.targets @my-web-app.rsp
+%msbuildexe% configure-my-web-app.targets @my-web-app.rsp %*
